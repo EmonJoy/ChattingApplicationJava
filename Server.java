ba7b5b7eaeca84b401cb9471aaf1d40b4cb8cd3d
MR. Morningstar <111835338+EmonJoy@users.noreply.github.com>
2024-06-03 14:48:22 +0600

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,177 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.sql.SQLOutput;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.net.*;
+
+public class Server  implements ActionListener {
+    JTextField text1;
+    JPanel a1;
+    static DataOutputStream dout;
+    static Box vertical = Box.createVerticalBox();
+    static JFrame f = new JFrame();
+
+    Server(){
+        f.setLayout(null);
+
+        JPanel p1 = new JPanel();
+        p1.setBackground(new Color(7,94,84));
+        p1.setBounds(0,0,450,70);
+        p1.setLayout(null);
+        f.add(p1);
+
+        // img saving
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
+        Image i2 = i1.getImage().getScaledInstance(25,25, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel back = new JLabel(i3);
+        back.setBounds(5,20,25,25);
+        p1.add(back);
+
+        back.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.exit(0);
+            }
+        });
+        // profile img ==>
+        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons/1.png"));
+        Image i5 = i4.getImage().getScaledInstance(50,50, Image.SCALE_DEFAULT);
+        ImageIcon i6 = new ImageIcon(i5);
+        JLabel profile = new JLabel(i6);
+        profile.setBounds(40,10,50,50);
+        p1.add(profile);
+
+        // video img
+        ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icons/video.png"));
+        Image i8 = i7.getImage().getScaledInstance(50,50, Image.SCALE_DEFAULT);
+        ImageIcon i9 = new ImageIcon(i8);
+        JLabel video = new JLabel(i9);
+        video.setBounds(300,20,30,30);
+        p1.add(video);
+
+        // name add
+        JLabel name = new JLabel("MR. Morningstar");
+        name.setBounds(110,15,200,18);
+        name.setForeground(Color.white);
+        name.setFont(new Font("SAN_SERIF", Font.BOLD, 13));
+        p1.add(name);
+
+
+        a1 = new JPanel();
+        a1.setBounds(5,75,440,570);
+        a1.setLayout(new BorderLayout());
+        f.add(a1);
+
+        // text adding
+        text1 = new JTextField();
+        text1.setBounds(5,655,310,40);
+        text1.setFont(new Font("SAN_SERIF" ,Font.PLAIN, 16));
+        f.add(text1);
+
+        // button
+        JButton send = new JButton("Send");
+        send.setBounds(320, 655, 100, 40);
+        send.setBackground(new Color(7,94,84));
+        send.addActionListener(this);
+        f.add(send);
+
+        // Size
+        f.setSize(450,700);
+        f.setLocation(200, 50);
+        f.setUndecorated(true);
+        f.getContentPane().setBackground(Color.WHITE);
+
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+    }
+    public void actionPerformed(ActionEvent e){
+        try {
+            String out = text1.getText();
+
+            JLabel output = new JLabel(out);
+
+            JPanel p2 = formatLable(out);
+
+
+            a1.setLayout(new BorderLayout());
+
+            JPanel right = new JPanel(new BorderLayout());
+            right.add(p2, BorderLayout.LINE_END);
+            vertical.add(right);
+            vertical.add(Box.createVerticalStrut(15));
+
+            a1.add(vertical, BorderLayout.PAGE_START);
+
+            dout.writeUTF(out);
+
+            f.repaint();
+            f.invalidate();
+            f.validate();
+            text1.setText("");
+        }catch (Exception ae){
+            ae.printStackTrace();
+        }
+    }
+
+    public static JPanel formatLable(String out){
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JLabel output = new JLabel(out);
+
+        panel.add(output);
+        output.setFont(new Font("Arial", Font.PLAIN, 16));
+        output.setBackground(new Color(37,211,102));
+        output.setOpaque(true);
+        output.setBorder(new EmptyBorder(15, 15, 15, 50));
+        panel.add(output);
+
+        Calendar cal= Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        JLabel time = new JLabel();
+        time.setText(sdf.format(cal.getTime()));
+        panel.add(time);
+
+
+        return panel ;
+    }
+
+
+    public static void main(String[] args) {
+        new Server();
+
+        try {
+            ServerSocket skt = new ServerSocket(6001);
+            while (true){
+                Socket s = skt.accept();
+                DataInputStream din = new DataInputStream(s.getInputStream());
+                DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+
+                while (true){
+                    String msg = din.readUTF();
+                    JPanel panel = formatLable(msg);
+
+                    JPanel left = new JPanel(new BorderLayout());
+                    left.add(panel, BorderLayout.LINE_START);
+                    vertical.add(left);
+                    f.validate();
+                }
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
+
+// https://youtu.be/CiEhC1Vvpco?si=CWpXPFQUAEvM0ZAG&t=723
